b61d4ebb1e924eeb6e6babb74ae04cd3c8243b40
admin <admin@localhost.localdomain>
2020-04-06 13:19:40 +0300

    update project 1.1

--- app/controller/pages.go
+++ app/controller/pages.go
@@ -9,10 +9,27 @@ import (
 
 func StartPage(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	//указываем путь к нужному файлу
-	path := filepath.Join("public", "html", "sp.html")
+	//path := filepath.Join("public", "html", "sp.html")
 
 	//создаем html-шаблон
-	tmpl, err := template.ParseFiles(path)
+	//tmpl, err := template.ParseFiles(path)
+        content :=  `<html lang="ru">
+	                     <head>
+		                       <meta charset="UTF-8">
+		                       <title>Title</title>
+		                   </head>
+		                   <body>
+		                       <h1>Приветствую тебя на стартовой странице этого сайта!</h1>
+		                       <ul>
+		                           <li>Первый элемент маркированного списка</li>
+			                         <li>Второй элемент маркированного списка</li>
+			                         <li>Третий элемент маркированного списка</li>
+			                         <li>Четвертый элемент маркированного списка</li>
+		                       </ul>
+		                   </body>
+		                   </html>`
+        //создаем html-шаблон
+	tmpl, err := template.New("example").Parse(content)
 	if err != nil {
 		http.Error(rw, err.Error(), 400)
 		return
